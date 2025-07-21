b3043fa272722e5634b8852ace18b6836616fb55
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2025-07-21 18:58:38 +0300

    changed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 export const metadata = {
-  title: 'My App',
-  description: 'Welcome to my app',
+  title: "TiTim Airlines",
+  description: "TiTim Airlines is a new airlines company",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>

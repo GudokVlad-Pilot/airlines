3a0ff9439dca7ee0d0bf65a8c48730321de636a2
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2025-07-28 22:30:08 +0300

    test state management working

--- src/app/[locale]/flights/page.tsx
+++ src/app/[locale]/flights/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+
+import { useStore } from "@/adapters/zustand/store";
+
 export default function FlightsPage() {
+  const { count, inc, reset } = useStore();
+
   return (
     <div>
       <h1>Flights</h1>
       <p>Welcome to the flights page!</p>
+      <span>{count}</span>
+      <div>
+        <button onClick={inc}>one up</button>
+        <button onClick={reset}>reset</button>
+      </div>
     </div>
   );
 }

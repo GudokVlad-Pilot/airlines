561beaabc72a9c9ed653381aed1726fbe766d6c8
GudokVlad-Pilot <vladislavpogudin.dev@gmail.com>
2025-08-20 20:59:26 +0300

    polishing adapter

--- src/adapters/adapter.ts
+++ src/adapters/adapter.ts
@@ -1,21 +1,21 @@
-import { sanityClient } from "./clients/sanity"
-import { pagesQuery } from "./queries"
-import { Page } from "./types"
+import { sanityClient } from "./clients/sanity";
+import { pagesQuery } from "./queries";
+import { Page } from "./types";
 
 export const adapters = {
   cms: () => {
     return {
       getPages: async (): Promise<Page[]> => {
         try {
-          const result = await sanityClient.fetch(pagesQuery) as Page[]
-          console.log(result)
-          console.log("I am getPages from Sanity")
-          return result
+          const result = (await sanityClient.fetch(pagesQuery)) as Page[];
+          console.log(result);
+          console.log("I am getPages from Sanity");
+          return result;
         } catch (error) {
-          console.error("Failed to fetch pages:", error)
-          return []
+          console.error("Failed to fetch pages:", error);
+          return [];
         }
-      }
-    }
-  }
-}
+      },
+    };
+  },
+};
